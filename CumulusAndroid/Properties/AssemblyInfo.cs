dad2576d63ccdbd9a7aa6e7d9f98868102c800db
Frank <fr1303@hotmail.fr>
2019-03-30 18:30:53 +0100

    build : ajustement de la version de production

--- CumulusAndroid/Properties/AssemblyInfo.cs
+++ CumulusAndroid/Properties/AssemblyInfo.cs
@@ -1,7 +1,12 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using Android.App;
+using System.Reflection;
 using System.Runtime.InteropServices;
-using Android.App;
+
+#if DEBUG
+[assembly: Application(Debuggable = true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -39,3 +44,4 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
