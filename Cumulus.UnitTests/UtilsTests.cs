b3caec77e32c804a43c43f6ec37b419a5e270764
Frank <fr1303@hotmail.fr>
2019-05-18 14:50:26 +0200

    test : création de la base des test unitaires

---
+++ Cumulus.UnitTests/UtilsTests.cs
@@ -0,0 +1,20 @@
+using Cumulus.Utility;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+
+namespace Cumulus.UnitTests
+{
+    public class UtilsTests
+    {
+        [Test]
+        public void GetPositionOnScreenByPercent_CenterOfScreen_IsTheCenter()
+        {
+            const float x = .5f;
+            const float y = .5f;
+
+            Vector2 result = Utils.GetPositionOnScreenByPercent(x, y);
+
+            Assert.That(result, Is.EqualTo(new Vector2(Utils.CENTER_SCREEN_HORIZONTAL, Utils.CENTER_SCREEN_VERTICAL)));
+        }
+    }
+}
